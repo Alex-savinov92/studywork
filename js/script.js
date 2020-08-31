3c32cb69f858ed0133b93783ba57779b728fdb66
Alex <examle@gmail.com>
2020-08-31 11:02:54 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+let number = 4.6;
+
+console.log(4/0);
+console.log('string' * 9);
+
+const persone = "Alex";
+
+const bool = true;
+
+console.log(something); //null
+
+
+let und;
+console.log(something); // undefined
+
+const obj = {
+    name: "John",
+    age: 25,
+    iaMarried: false
+};
+
+console.log(obj.name);
+//console.log(obj['name']);
+
+
+
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', [], {}];
+console.log(arr[1]);
---
+++ js/scriptles9.js
@@ -0,0 +1,28 @@
+"use strict";
+
+console.log('arr' + "- object" );
+
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+//++incr;
+//--decr;
+
+console.log(incr);
+console.log(decr);
+
+
+console.log(2*4 === 8);
+
+
+
+const isChecked = true;
+const isClose = false;
+console.log(isChecked || !isClose);
+
+
+
